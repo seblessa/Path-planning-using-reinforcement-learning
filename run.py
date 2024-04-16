2261e6bb3e12931f89d7815f82859f631cc7c6a4
seblessa <sebastiaolessa2002@outlook.com>
2024-04-16 17:20:59 +0100

    Base world upload

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 # from package import
 
+import math
+from controller import Robot
+from package import cmd_vel, move_forward, rotate
+
+
 if __name__ == '__main__':
-    print('Hello, World!')    
+    # Create the Robot instance.
+    robot: Robot = Robot()
+
+    # tile length in meters
+    d = 0.125 / 2
+
+    while True:
+        move_forward(robot, d * 2, 0.1)
+        rotate(robot, math.pi / 2, 0.5)
+
